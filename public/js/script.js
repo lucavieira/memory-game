3262c9decf0e33f393454f5f4a228b025b6af962
lucavieira <lukasveras14@gmail.com>
2022-03-31 12:12:31 -0300

    Repetição de codigo retiradas. Funcoes criadas para Remover clicks do array e para esconder o card.

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,8 @@
 let cards = document.querySelector('.cards')
-let select = document.querySelector('#dificult')
 
 let score = 0
 let clicks = []
-let cards_images = [
+let cards_images_easy = [
     'public/images/cards/book_angle.png', 
     'public/images/cards/coffin.png', 
     'public/images/cards/eye_angle.png',
@@ -21,55 +20,77 @@ let cards_images = [
     'public/images/cards/voodoo_doll.png',
     'public/images/cards/witch.png',
     'public/images/cards/witchs_hat.png',
-    'public/images/cards/zombie.png'
+    'public/images/cards/zombie.png',
 ]
 
-select.addEventListener('change', () => {
-    if(select.value == 'easy') {
-        shuffleArray(cards_images)
-        createCards(18)
+$('#dificult').on('change', () => {
+    score = 0
+    let dificult = $('#dificult')[0].value
+    if(dificult == 'easy') {
+        shuffleArray(cards_images_easy)
+        createCards(18, dificult)
     }
 
-    if(select.value == 'medium') {
-        createCards(24)
+    if(dificult == 'medium') {
+        createCards(24, dificult)
     }
 
-    if(select.value == 'hard') {
-        createCards(30)
+    if(dificult == 'hard') {
+        createCards(30, dificult)
     }
 })
 
-function createCards(quantity) {
+function createCards(quantity, dificult) {
     if(cards.hasChildNodes()) {
-        if(cards.childElementCount < quantity) {
-            let new_quantity = quantity - cards.childElementCount
-            for(let index = 0; index < new_quantity; index++) {
-                var card = document.createElement('div')
-                card.classList.add('card')
-                card.id = index
-                cards.appendChild(card)
-            }
+        let cards_length = cards.childNodes.length
+        for(let index = 0; index < cards_length; index++) {
+            $(`#${index}`).remove(`#${index}`)
         }
 
-        if(cards.childElementCount > quantity) {
-            let new_quantity = cards.childElementCount - quantity
-            for(let index = 0; index < new_quantity; index++) {
-                cards.removeChild(cards.lastChild)
+        setTimeout(() => {
+            for(let index = 0; index < quantity; index++) {
+                if(dificult == 'easy') {
+                    $('.cards').append(`<div class="card" id="${index}" onClick=showCard(this.id)> <img src="${cards_images_easy[index]}" /> </div>`)
+                }
+
+                // if(dificult == 'medium') {
+                //     $('.cards').append(`<div class="card" id="${index}" onClick=showCard(this.id)> <img src="${cards_images_medium[index]}" /> </div>`)
+                // }
+
+                // if(dificult == 'hard') {
+                //     $('.cards').append(`<div class="card" id="${index}" onClick=showCard(this.id)> <img src="${cards_images_hard[index]}" /> </div>`)
+                // }
+
+                $('.card').css('background-image', "url(public/images/cards/card.png)")
             }
-        }
+        }, 200)
         
     } else {
         for(let index = 0; index < quantity; index++) {
-            $('.cards').append(`<div class="card" id="${index}" onClick=showCard(this.id)> <img src="${cards_images[index]}" /> </div>`)
+            $('.cards').append(`<div class="card" id="${index}" onClick=showCard(this.id)> <img src="${cards_images_easy[index]}" /> </div>`)
             $('.card').css('background-image', "url(public/images/cards/card.png)")
         }
     }
 }
 
+// Embaralha o array de imagens
 function shuffleArray(cards_array){
     cards_array.sort(()=> Math.random() - 0.5);
 }
 
+// Função para esconder o card
+function hideCard(clicks) {
+    clicks.forEach(id => {
+        $(`#${id}`).css('background-image', 'url(public/images/cards/card.png)')
+        $(`#${id} img`).css('display', 'none') 
+    });
+}
+
+// Limpa o array de clicks
+function removeClicks(clicks) {
+    clicks.pop()
+    clicks.pop()
+}
 
 function showCard(id) {
     if(clicks.length < 2) {
@@ -79,23 +100,23 @@ function showCard(id) {
     }
 
     if(clicks.length == 2) {
-        if(($(`#${clicks[0]} img`)[0].src == $(`#${clicks[1]} img`)[0].src) == false) {
-            setTimeout(() => {
-                $(`#${clicks[0]}`).css('background-image', 'url(public/images/cards/card.png)')
-                $(`#${clicks[0]} img`).css('display', 'none')
-                $(`#${clicks[1]}`).css('background-image', 'url(public/images/cards/card.png)')
-                $(`#${clicks[1]} img`).css('display', 'none')
-                clicks.pop()
-                clicks.pop()
-            }, 1000)
-        } else {
-            score += 1
-            console.log(score)
-            if(select.value == 'easy' && score == 9) {
-                console.log('FINISHED')
+        if(clicks[0] != clicks[1]) {
+            if(($(`#${clicks[0]} img`)[0].src == $(`#${clicks[1]} img`)[0].src) == false) {
+                setTimeout(() => {
+                    hideCard(clicks)
+                    removeClicks(clicks)
+                }, 1000)
+            } else {
+                score += 1
+                console.log(score)
+                if($('#dificult')[0].value == 'easy' && score == 9) {
+                    console.log('FINISHED')
+                }
+                removeClicks(clicks)
             }
-            clicks.pop()
-            clicks.pop()
+        } else {
+            hideCard(clicks)
+            removeClicks(clicks)
         }
     }
 
